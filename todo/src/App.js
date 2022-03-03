bb657602c098bc63e4c05a945b06c69437009c98
KorzhovVD <svalesik@mail.ru>
2022-03-03 16:59:30 +0300

    create React-элемент

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,34 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, lenght} from './number.js'
+import React from 'react'
 
-const flag = true;
-const val = 777;
+const element = [<div>
+  Hello, World
+</div>, 
+<a href="">
+  Перейти по ссылке
+</a> 
+];
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-       <p style={{
-        color: 'blue'}}>
-          count*lenght = {count*lenght}
-        </p>              
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element}
--- todo/src/number.js
+++
@@ -1,4 +0,0 @@
-const count = 20
-const lenght = 12
-
-export {count, lenght}
